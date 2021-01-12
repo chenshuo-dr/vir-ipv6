16940b83b31c338d0f616214cc502123cf91b191
chenshuo-as <44311021+chenshuo-as@users.noreply.github.com>
2021-01-12 18:15:25 +0800

    Update vir-la8.sh

--- vir-la8.sh
+++ vir-la8.sh
@@ -7,11 +7,11 @@ sysctl -p
 sleep 1
 echo "auto he-ipv6" >> /etc/network/interfaces
 echo "iface he-ipv6 inet6 v4tunnel" >> /etc/network/interfaces
-echo "address 2001:470:c:7a6::2" >> /etc/network/interfaces
+echo "address 2001:470:66:643::2" >> /etc/network/interfaces
 echo "netmask 64" >> /etc/network/interfaces
-echo "endpoint 66.220.18.42" >> /etc/network/interfaces
+echo "endpoint 64.62.134.130" >> /etc/network/interfaces
 echo "local 107.175.21.170" >> /etc/network/interfaces
 echo "ttl 255" >> /etc/network/interfaces
-echo "gateway 2001:470:c:7a6::1" >> /etc/network/interfaces
+echo "gateway 2001:470:66:643::1" >> /etc/network/interfaces
 sleep 1
 exit
