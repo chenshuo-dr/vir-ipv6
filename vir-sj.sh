a1b7cd308352ec8a5bf6a3d0d0e2be9cb5a1d5d2
chenshuo-as <44311021+chenshuo-as@users.noreply.github.com>
2021-02-28 12:48:15 +0800

    Update vir-sj.sh

--- vir-sj.sh
+++ vir-sj.sh
@@ -7,12 +7,12 @@ sysctl -p
 sleep 1
 echo "auto he-ipv6" >> /etc/network/interfaces
 echo "iface he-ipv6 inet6 v4tunnel" >> /etc/network/interfaces
-echo "address 2001:470:1f04:243::2" >> /etc/network/interfaces
+echo "address 2001:470:66:669::2" >> /etc/network/interfaces
 echo "netmask 64" >> /etc/network/interfaces
-echo "endpoint 72.52.104.74" >> /etc/network/interfaces
+echo "endpoint 64.62.134.130" >> /etc/network/interfaces
 echo "local 192.3.117.239" >> /etc/network/interfaces
 echo "ttl 255" >> /etc/network/interfaces
-echo "gateway 2001:470:1f04:243::1" >> /etc/network/interfaces
+echo "gateway 2001:470:66:669::1" >> /etc/network/interfaces
 /etc/init.d/networking restart
 sleep 1
 exit
